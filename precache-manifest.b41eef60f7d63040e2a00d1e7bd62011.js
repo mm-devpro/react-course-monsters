2956af9b8b5dc6c02ac5887d5df360aed00dfd1d
mm-devpro <mickael@mm-devpro.com>
2020-05-21 16:06:28 +0200

    Updates

---
+++ precache-manifest.b41eef60f7d63040e2a00d1e7bd62011.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9800db296ccfbe3a646834c6990e97bb",
+    "url": "/react-course-monsters/index.html"
+  },
+  {
+    "revision": "09c725728965810192db",
+    "url": "/react-course-monsters/static/css/main.827669fe.chunk.css"
+  },
+  {
+    "revision": "12240f73676d8f2fbd85",
+    "url": "/react-course-monsters/static/js/2.a3c82928.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-course-monsters/static/js/2.a3c82928.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "09c725728965810192db",
+    "url": "/react-course-monsters/static/js/main.357bc361.chunk.js"
+  },
+  {
+    "revision": "b682f1c736fc7dd05439",
+    "url": "/react-course-monsters/static/js/runtime-main.a6775495.js"
+  }
+]);
